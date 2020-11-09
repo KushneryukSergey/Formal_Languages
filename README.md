fba8249a0afac7ab6aec6b83b95ca4a8e0c4d6ba
KushneryukSergey <kushneriuk.ss@phystech.edu>
2020-11-10 01:57:57 +0300

    README slightly improved

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Hometasks for "Formal Languages" course in Moscow Institute of Physics and Techn
 
 
 ## Projects
-### [Automata project](https://github.com/KushneryukSergey/Formal_Languages/tree/automata/automata)
+### [Automata project](https://github.com/KushneryukSergey/Formal_Languages/tree/main/automata)
 Library for working with NFA, converting them to DFA, complete and min DFAs. Output with code for LaTeX />  to create minimum DFA and to create graph of automaton is supported
 
 > #### See further project soon
---
+++ workshop1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/skushneryuk/study/formal_languages/workshop1
+# Build directory: /home/skushneryuk/study/formal_languages/workshop1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "/home/skushneryuk/study/formal_languages/workshop1/bin/tests")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/skushneryuk/study/formal_languages/workshop1/CMakeLists.txt;16;add_test;/home/skushneryuk/study/formal_languages/workshop1/CMakeLists.txt;0;")
