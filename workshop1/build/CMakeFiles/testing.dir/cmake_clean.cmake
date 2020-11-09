559e9c525c5d39e2d15a10366da8f971f410b1c8
KushneryukSergey <kushneriuk.ss@phystech.edu>
2020-11-10 01:58:47 +0300

    README slightly improved

--- workshop1/build/CMakeFiles/testing.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/testing"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
